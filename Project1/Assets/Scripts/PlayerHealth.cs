c2e34ca2cde2784671f12b6cb036f807aec6f314
danny-to7 <danny.to@uoit.net>
2019-03-05 00:02:30 -0500

    Project1
    Single-level platformer

---
+++ Project1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] public int playerHP = 4;
+    [SerializeField] public int invulnerableTime = 3;
+    public bool playerDead;
+    private Animator animator;
+
+    public Image[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerDead = false;
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < playerHP)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        //check if player falls off map
+        if (gameObject.transform.position.y < -10)
+        {
+            playerDead = true;
+            StartCoroutine(Die());
+        }
+    }
+
+    public void GetHit()
+    {
+        playerHP--;
+        StartCoroutine(HitBlinker(invulnerableTime));
+        Debug.Log(playerHP);
+        if (playerHP <= 0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Level1");
+        playerHP = 5;
+        yield return null;
+    }
+
+    IEnumerator HitBlinker(float invulnTime)
+    {
+        int enemyLayer = LayerMask.NameToLayer("Enemy");
+        int playerLayer = LayerMask.NameToLayer("Player");
+        Physics2D.IgnoreLayerCollision(enemyLayer, playerLayer);
+        animator.SetLayerWeight(1, 1);
+
+        yield return new WaitForSeconds(invulnTime);
+        Physics2D.IgnoreLayerCollision(enemyLayer, playerLayer, false);
+        animator.SetLayerWeight(1, 0);
+    }
+}
---
+++ Project1/Assets/Sprites/Enemies/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] public float speed = -1f;
+    [SerializeField] public float range = 3f;
+
+    public LayerMask enemyMask;
+    public Transform origin;
+    Vector2 dir = new Vector2(-1,0);
+    Rigidbody2D myBody;
+    Transform myTransform;
+    float myWidth;
+    
+    void Start()
+    {
+        myTransform = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+    }
+    
+    void FixedUpdate()
+    {
+        RaycastHit2D cast = Physics2D.Raycast(origin.position, dir, range);
+        if (cast == true)
+        {
+            if (cast.collider.CompareTag("Ground"))
+            {
+            }
+        } else
+        {
+            speed *= -1;
+            dir *= -1;
+            Flip();
+        }
+
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+   /*     Vector2 lineCastPos = myTransform.position - myTransform.right * myWidth;
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+
+        //rotate when enemy meets edge of platform
+        if (!isGrounded)
+        {
+            Vector3 currentRotation = myTransform.eulerAngles;
+            currentRotation.y += 180;
+            myTransform.eulerAngles = currentRotation;
+        }
+
+        Vector2 myVelocity = myBody.velocity;
+        myVelocity.x = -myTransform.right.x * speed;
+        myBody.velocity = myVelocity; */
+    }
+
+    private void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
